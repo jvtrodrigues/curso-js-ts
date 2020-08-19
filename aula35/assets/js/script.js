1cd48aaca195afa0713996a8cceafd7c883921f8
José Rodrigues <josehgunner@gmail.com>
2020-08-19 15:53:20 -0300

    exercício for

---
+++ aula35/assets/js/script.js
@@ -0,0 +1,23 @@
+const elementos = [
+  {tag: 'p', texto: 'Frase 1'},
+  {tag: 'div', texto: 'Frase 2'},
+  {tag: 'section', texto: 'Frase 3'},
+  {tag: 'footer', texto: 'Frase 4'},
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+function montaElementos() {
+  for (let i = 0; i < elementos.length; i++) {
+    const { tag, texto } = elementos[i];
+    const tagCriada = document.createElement(tag);
+    const textoCriado = document.createTextNode(texto);
+    tagCriada.appendChild(textoCriado);
+    div.appendChild(tagCriada);
+  }
+}
+
+montaElementos();
+
+container.appendChild(div);
