1f2b8f95e7c5ca75ae9f625b583a4a47d6881e5f
José Rodrigues <josehgunner@gmail.com>
2020-08-19 12:29:08 -0300

    for clássico

---
+++ aula34/index.js
@@ -0,0 +1,9 @@
+/*for (let i = 0; i <= 10; i++) {
+  console.log(`${i} é ${i % 2 == 0 ? 'par': 'ímpar'}`);
+}*/
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Índice ${i}`,frutas[i]);
+}
